d854413fcf1f924c6949e67ed1aa8de125f050af
rcb1409 <ravichandubollepalli@gmail.com>
2024-09-21 03:22:13 +0000

    made a pytest case fail

--- test_myfile.py
+++ test_myfile.py
@@ -8,7 +8,7 @@ def test_addition():
     assert calculator(x,y,'+') == 3
 
 def test_division():
-    assert calculator(10,5,'/') == 2
+    assert calculator(10,5,'/') == 3
 
 def test_zero_division():
     with pytest.raises(ZeroDivisionError, match= "cannot divide by zero"):
